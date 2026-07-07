0ec209819e26c27289264e8b61f912690f4ea130
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-07-07 23:29:57 +0200

    bash: 6/95

---
+++ bash/difference-of-squares/difference_of_squares.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+square_of_sum () {
+    echo $(( ($1 * ($1 + 1) / 2) ** 2 ))
+}
+
+sum_of_squares() {
+    echo $(( ($1 *  ($1 + 1) * (2 * $1 + 1)) / 6 ))
+}
+
+difference() {
+    echo $(( $(square_of_sum "$1") - $(sum_of_squares "$1") ))
+}
+
+"$1" "$2"
